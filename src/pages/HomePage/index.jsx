ae9158da720faccf232115b282673c1833d4bbe0
alfahmist <gst.alfahmi@gmail.com>
2024-05-05 16:49:05 +0700

    Menampilkan category pada homepage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -6,25 +6,35 @@ const getProducts = () => {
 	return axios.get('https://dummyjson.com/products?limit=0');
 };
 
+const getCategories = () => {
+	return axios.get('https://dummyjson.com/products/categories');
+};
+
 const index = () => {
 	const [data, setData] = useState([]);
+	const [categories, setCategories] = useState([]);
 	useEffect(() => {
 		getProducts().then((response) => {
 			console.log(response.data.products);
 			setData(response.data.products);
 		});
+
+		getCategories().then((response) => {
+			console.log(response.data);
+			setCategories(response.data);
+		});
 	}, []);
 	return (
 		<>
 			<ProductList products={data.slice(0, 10)} title={'Hightlight Products'} />
-			<ProductList
-				products={data.filter((x) => x.category === 'smartphones')}
-				title={'Category Smartphone'}
-			/>
-			<ProductList
-				products={data.filter((x) => x.category === 'laptops')}
-				title={'Category Laptop'}
-			/>
+			{categories.slice(0, 5).map((category) => {
+				return (
+					<ProductList
+						products={data.filter((x) => x.category === category)}
+						title={`Category ${category}`}
+					/>
+				);
+			})}
 		</>
 	);
 };
