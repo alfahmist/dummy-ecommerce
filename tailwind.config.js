36730820e6093940cda2a807aee5809494ae0a46
alfahmist <gst.alfahmi@gmail.com>
2024-05-05 10:10:17 +0700

    setting config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,jsx}', './src/components/**/*.{jsx}'],
+	content: ['./src/pages/**/*.jsx', './src/components/**/*.jsx'],
 	theme: {
 		extend: {
 			fontFamily: {
