3d3e152b7a0eefe6a9dfa14baef625e6970fcb09
alfahmist <gst.alfahmi@gmail.com>
2024-05-05 10:14:23 +0700

    delete logo pada navigasi

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -32,11 +32,6 @@ const index = () => {
 							</div>
 							<div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
 								<div className='flex flex-shrink-0 items-center gap-2 sm:gap-0'>
-									<img
-										className='h-8 w-auto self-start'
-										src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500'
-										alt='Your Company'
-									/>
 									<div className='sm:hidden'>
 										<SearchBar />
 									</div>
