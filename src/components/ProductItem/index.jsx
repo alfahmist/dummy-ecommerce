a31280ec71c034043d2a62e9fc25f8d409cbff4b
alfahmist <gst.alfahmi@gmail.com>
2024-05-05 16:32:55 +0700

    fix component productList dan productItem

--- src/components/ProductItem/index.jsx
+++ src/components/ProductItem/index.jsx
@@ -5,21 +5,28 @@ const index = (props) => {
 	return (
 		<div className='group relative'>
 			<div className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80'>
-				<img
-					src={product.thumbnail}
-					alt={product.imageAlt}
-					className='h-full w-full object-cover object-center lg:h-full lg:w-full'
-				/>
+				<Link to={`/products/detail/${product.id}`}>
+					<img
+						src={product.thumbnail}
+						alt={product.imageAlt}
+						className='h-full w-full object-cover object-center lg:h-full lg:w-full'
+					/>
+				</Link>
 			</div>
 			<div className='mt-4 flex justify-between'>
 				<div>
 					<h3 className='text-sm text-gray-700'>
 						<Link to={`/products/detail/${product.id}`}>
-							<span aria-hidden='true' className='absolute inset-0' />
+							{/* <span aria-hidden='true' className='absolute inset-0' /> */}
 							{product.title}
 						</Link>
 					</h3>
-					<p className='mt-1 text-sm text-gray-500'>{product.category}</p>
+					<Link
+						to={`/category/${product.category}`}
+						className='mt-1 text-sm text-gray-500 hover:text-black'
+					>
+						{product.category}
+					</Link>
 					<p className='mt-1 text-sm text-gray-500'>{product.brand}</p>
 				</div>
 				<p className='text-sm font-medium text-gray-900'>$ {product.price}</p>
--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -2,7 +2,6 @@ import { ProductItem } from '..';
 
 const index = (props) => {
 	const { products, title } = props;
-	console.log('props');
 	return (
 		<div className='bg-white'>
 			<div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
