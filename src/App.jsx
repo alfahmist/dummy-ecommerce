8036ac4d5ae6e3fe61d8d19b426dfe9e4e1831d6
alfahmist <gst.alfahmi@gmail.com>
2024-05-05 16:34:54 +0700

    Menambahkan pages NotFound

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { DetailPage, HomePage, ProductPage, CategoryPage } from './pages';
+import {
+	DetailPage,
+	HomePage,
+	ProductPage,
+	CategoryPage,
+	SearchPage,
+} from './pages';
 import { Navigation } from './components';
 function App() {
 	return (
@@ -11,6 +17,7 @@ function App() {
 					<Route path='/products' element={<ProductPage />} />
 					<Route path='/products/detail/:productId' element={<DetailPage />} />
 					<Route path='/category/:categoryName' element={<CategoryPage />} />
+					<Route path='/search/:name' element={<SearchPage />} />
 				</Routes>
 			</BrowserRouter>
 		</>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,14 @@ import HomePage from './HomePage';
 import ProductPage from './ProductPage';
 import DetailPage from './DetailPage';
 import CategoryPage from './CategoryPage';
+import SearchPage from './SearchPage';
+import NotFoundPage from './NotFoundPage';
 
-export { HomePage, ProductPage, DetailPage, CategoryPage };
+export {
+	HomePage,
+	ProductPage,
+	DetailPage,
+	CategoryPage,
+	SearchPage,
+	NotFoundPage,
+};
